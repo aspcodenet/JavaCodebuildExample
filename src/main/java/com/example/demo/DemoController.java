9f3aa9012eefed28db229abeee4e4c84ac8c73c5
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-03-28 19:55:21 +0200

    Added XC70

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -16,6 +16,7 @@ public class DemoController {
     public List<Car> getall() {
         var l = new ArrayList<Car>();
         
+        l.add(new Car("Volvo", "XC70", "Blå", 2008));
         l.add(new Car("Volvo", "XC60", "Svart", 2015));
         l.add(new Car("Volvo", "240", "Röd", 1985));
         l.add(new Car("Renault", "Megane", "Svart", 2018));
