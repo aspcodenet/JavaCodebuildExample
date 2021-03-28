52d083cf797c41a38a33a4977d092107054e8107
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-03-28 18:56:58 +0200

    first commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class DemoController {
+    @GetMapping(value="/car")
+    @CrossOrigin()
+    public List<Car> getall() {
+        var l = new ArrayList<Car>();
+        
+        l.add(new Car("Volvo", "XC60", "Svart", 2015));
+        l.add(new Car("Volvo", "240", "Röd", 1985));
+        l.add(new Car("Renault", "Megane", "Svart", 2018));
+        return l;
+        
+    }
+    
+}
